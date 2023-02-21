c8b9dc100899b580404601522f27da731d9510ff
Shoaib <shoaibahmed27@gmail.com>
2023-02-22 00:19:00 +0500

    FC

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import agCoreUi from "ag-ui-vite";
 
-createApp(App).mount('#app')
+createApp(App).use(agCoreUi).use(router).mount('#app')
